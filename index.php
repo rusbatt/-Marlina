d691b926147539c8927f63626bfff15a20976254
ruslan-batt <battalowrus@yandex.kz>
2019-07-22 00:34:03 +0600

    %conect to db%

--- index.php
+++ index.php
@@ -5,15 +5,12 @@
 	<title>Homepage</title>
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
-<body>
-	
-	
+<body>	
 	<div class="container">
 		<div class="row">
 			<div class="col-md-12">
 				<h1>User management</h1>
-				<a href="create.html" class="btn btn-success">Add User</a>
-				
+				<a href="create.html" class="btn btn-success">Add User</a>				
 				<table class="table">
 					<thead>
 						<tr>
@@ -23,10 +20,42 @@
 							<th>Actions</th>
 						</tr>
 					</thead>
-
 					<tbody>
 						<?php 
-							/*$arrUserAll = [
+							//подключаемся к БД (создав объект)
+							$pdo = new PDO('mysql:host=127.0.0.1;dbname=bd_marlin;charset=utf8;', 'root', 'OtsbSslgEEMCovEj');
+							
+							$sql = "SELECT * FROM `users`"; // "SELECT * FROM `users`" - это команда из языка sql, которя является значением переменной $sql и само по себе ни каких действии не производит, для этого нам нужена функция query()  
+							$statement = $pdo->query($sql);	// это функция выбора таблицы как и "SELECT * FROM `users`" только в PHP.										
+						 $users = $statement->fetchAll();// а эта функция fetchAll() с помощью которой мы говорим что из выбраной таблиции нам нужны все пользователи.
+
+
+						foreach ($users as $elem):
+						?>
+						<tr>					
+							<td><?php echo $elem['id']; ?></td>
+							<td><?php echo $elem['Username']; ?></td>
+							<td><?php echo $elem['Email']; ?></td>
+						<td>
+							<a href="edit.html" class="btn btn-warning">Edit</a>
+							<a href="#" onclick="return confirm('are you sure?')" class="btn btn-danger">Delete</a>
+						</td>
+						</tr>
+						<?php
+							endforeach;
+						?>
+					
+						  
+						
+					</tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
+
+<!--$arrUserAll = [
 								['id' => '1',
 									'Username' => "Иванов",
 									'Email' => "ivanv@mail.ru"
@@ -44,36 +73,4 @@
 								 'Email' => "pescasctrscsc@mail.ru"
 								]
 
-							];*/
-
-							//подключаемся к БД 
-
-							$pdo = new PDO('mysql:host=127.0.0.1;bdname:bd_marlin;charset=utf8', 'root', 'OtsbSslgEEMCovEj');
-							$sql = 'SELECT * FROM users';
-							$statement = $pdo->query($sql);
-							$users = $statement->fetchAll();
-
-
-							foreach ($users as $elem):
-						?>
-						<tr>					
-							<td><?php echo $elem['id']; ?></td>
-							<td><?php echo $elem['Username']; ?></td>
-							<td><?php echo $elem['Email']; ?></td>
-						<td>
-							<a href="edit.html" class="btn btn-warning">Edit</a>
-							<a href="#" onclick="return confirm('are you sure?')" class="btn btn-danger">Delete</a>
-						</td>
-						</tr>
-						<?php
-							endforeach;
-						?>
-						  
-						
-					</tbody>
-				</table>
-			</div>
-		</div>
-	</div>
-</body>
-</html>
+							];-->
