9ba26fac92eee63f05d5a2b586e3470b418304a0
ruslan-batt <battalowrus@yandex.kz>
2019-07-28 19:50:29 +0600

    %sql inection%

--- handler.php
+++ handler.php
@@ -12,12 +12,17 @@
 
     if ($name == true and $mail == true and $pass == true) {
         //с помощью Sql запросза добавили пользователя в БД 
-        $pdo->query("INSERT INTO ".$db_table." (`id`, `Username`, `Email`, `password`) VALUES (NULL, '$name', '$mail', '$pass')");
+        $resultSelect = $pdo->prepare("INSERT INTO ".$db_table." (`Username`, `Email`, `password`) VALUES (:name, :mail, :pass)");// prepare -подготовка запроса
+        $resultSelect->bindParam(':name', $name); // bind_param подставляем нужные переменные
+        $resultSelect->bindParam(':mail', $mail);
+        $resultSelect->bindParam(':pass', $pass);
 
-        $nameFile = $_FILES['imag']['name'];
-        $tmp_name = $_FILES['imag']['tmp_name'];
+        $resultSelect->execute(); //выводим запрос 
 
-        move_uploaded_file($tmp_name, 'uploads/' . uniqid() . $nameFile);
+        $nameFile = $_FILES['imag']['name'];//создаем переменную где значение это имя загруженного файла
+        $tmp_name = $_FILES['imag']['tmp_name'];//создаем переменную где значеник это временное хранилище файла
+
+        move_uploaded_file($tmp_name, 'uploads/' . uniqid() . $nameFile);//функция которя из временного хранилеща передает файл в созданую нами папку uploads и присваевает имени файла уникальное имя
 
 
 
